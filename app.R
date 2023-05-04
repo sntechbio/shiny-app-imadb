45c630684c72952e29b6b2f5f57adf49efc529f5
leandro-lcsistemas <leandro.nascimento@lcsistemas.com>
2023-05-04 00:13:51 -0300

    adição de abas e ajustes

--- app.R
+++ app.R
@@ -1,5 +1,8 @@
 library(shiny)
 library(corrplot)
+library(ggradar)
+library(scales)
+library(dplyr)
 
 ui <- fluidPage(
   
@@ -15,49 +18,83 @@ ui <- fluidPage(
     )
   ),
   
-  titlePanel(strong("Imadb App")),
+  titlePanel("imadb data viz"),
   
   sidebarLayout(
     sidebarPanel(
       
       helpText("Analysis of the database or external files"),
       
-      fileInput(inputId = "input_data_ext", label = "Input Data",
-                accept = ".csv",
-                buttonLabel = "Browse..."),
-      
-      helpText("Clustering order of correlations"),
-      
-      selectInput("order", label = "Order", 
-                  choices = c("original", "AOE", "FPC", "hclust", "alphabet"), 
-                  selected = "hclust"),
-      
-      textInput(inputId = "title_correlation_plot", label = "Graph Title"),
-      
-      sliderInput(inputId = "font_size", label = "Font Size", value = 1.5, min = 0.5, max = 3, step = 0.7, width = "300px"),
-      
-      sliderInput(inputId = "plot_size", label = "Plot Size", value = 600, min = 400, max = 900, step = 150, width = "300px"),
-      
-      actionButton(inputId = "analyze_data_button", label = "Analisys Data")
+      tabsetPanel(
+        
+        tabPanel("correlation", 
+                 
+                 
+                 h3("Correlation Analisys"),
+                 fileInput(inputId = "input_data_corr", label = "Input Data",
+                           accept = ".csv",
+                           buttonLabel = "Browse..."),
+                 helpText("Clustering order of correlations"),
+                 selectInput("order", label = "Order", 
+                             choices = c("original", "AOE", "FPC", "hclust", "alphabet"), 
+                             selected = "hclust"),
+                 textInput(inputId = "title_correlation_plot", label = "Graph Title"),
+                 sliderInput(inputId = "font_size", label = "Font Size", value = 1.5, min = 0.5, max = 3, step = 0.7, width = "300px"),
+                 sliderInput(inputId = "plot_size", label = "Plot Size", value = 600, min = 400, max = 900, step = 100, width = "300px"),
+                 actionButton(inputId = "analyze_data_button", label = "Analisys Data")
+                 
+                 ),
+        
+        tabPanel("radar",
+                 
+                 h3("Radar Chart"),
+                 fileInput(inputId = "input_data_radar", label = "Input Data",
+                           accept = ".csv",
+                           buttonLabel = "Browse..."),
+                 
+                 selectInput("radarVariables", choices = NULL,
+                             label = "variables", selected = NULL)
+                 
+                 
+                 )
+        
+            ),
       
       ),
     
-    mainPanel(div(id = "correlation_plot_container",
-                  plotOutput("correlationPlot", width = "600px", height = "600px")))
+    
+    mainPanel(
+      
+      tabsetPanel(type = "tabs",
+                  
+                  tabPanel("Correlation Plot", div(id = "correlation_plot_container",
+                                              plotOutput("correlationPlot", width = "600px", height = "650px"))
+                           ),
+                  
+                  tabPanel("Radar Chart", plotOutput("radarPlot")),
+                  
+                  tabPanel("Table", DT::dataTableOutput("tableData"))
+                  
+                  ),
+      
+      
+      
+      
+      )
     
   )
   
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  # correlations -----------------------
   
   data_input <- reactive({
-    req(input$input_data_ext)
-    data_frame <- read.csv(input$input_data_ext$datapath)
+    req(input$input_data_corr)
+    data_frame <- read.csv(input$input_data_corr$datapath)
     data_frame <- replace(data_frame, is.na(data_frame), 0)
   })
-  
-  
   
   observeEvent(input$analyze_data_button, {
     
@@ -75,10 +112,36 @@ server <- function(input, output) {
       
     }, height = input$plot_size, width = input$plot_size)
     
+    output$tableData <- DT::renderDataTable({data_input()})
+    
   })
   
+  # radar --------------------------------------------------->
   
+  observeEvent(input$input_data_radar, {
+    
+    df <- read.csv(input$input_data_radar$datapath)
+    df <- replace(df, is.na(df), 0)
+    
+    updateSelectInput(
+      session,
+      "radarVariables",
+      choices = names(df),
+      selected = names(df)[1]
+    )
+    
+    df <- df %>% mutate_all(as.numeric)
+    
+    output$radarPlot <- renderPlot({
+      req(df)
+      ggradar(scale(df), aes_string(x = input$radarVariables, y = input$radarVariables))
+    })
+    
+  })
   
+  # end -------------------------------------------------------->
+  
+
 }
 
 shinyApp(ui = ui, server = server)
